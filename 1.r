86c6cc58b297908beaad87fcb36cad75fa56d155
izeh <31554814+izeh@users.noreply.github.com>
2018-12-05 13:16:02 -0600

    Add files via upload

---
+++ 1.r
@@ -0,0 +1,15 @@
+source("https://raw.githubusercontent.com/rnorouzian/i/master/i.r")
+
+d <- seq(.1, .5, .05)
+
+plot(1, xlim = range(d), ylim = c(0, 1600), xlab = "Cohen's d", ylab = "Group Sample Size", font.lab = 2)
+axis(1, at = c(.15, .361), col.axis = "purple")
+
+n <- power.t(d = d, power = .8)$n1
+lines(d, n, type = "b", pch = 16, col = 4, cex = 1.6)
+m <- plan.t.ci(d, width = .5)$n1
+lines(d, m, type = "o", col = 2, pch = 15, cex = 1.2, lty = 3)
+text(d, n, n, pos = 3, col = 4)
+text(d, m, m, pos = 1, col = 2)
+
+legend("topright", c("PAA", "AESE"), pch = c(19, 15), col = c(4, 2), lty = c(1, 3), text.font = 2, bty = "n")
